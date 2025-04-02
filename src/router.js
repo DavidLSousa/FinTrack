1a598c38003d594e6bf5036d17b08bf318259632
David Sousa <davidlsousa.dev@gmail.com>
2025-04-01 21:23:46 -0300

    add homepage

--- src/router.js
+++ src/router.js
@@ -4,8 +4,10 @@ import AddTicketsPage from './components/views/AddTicketsPage.vue'
 import AllTicketsPage from './components/views/AllTicketsPage.vue'
 import DashboardPage from './components/views/DashboardPage.vue'
 import NewsPage from './components/views/NewsPage.vue'
+import HomePage from './components/views/HomePage.vue'
 
 const routes = [
+  { path: '/', component: HomePage },
   { path: '/addTicket', component: AddTicketsPage },
   { path: '/allTicket', component: AllTicketsPage },
   { path: '/dashboard', component: DashboardPage },
